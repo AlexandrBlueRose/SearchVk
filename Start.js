4edf84c22ca65eb6f7e93d8c5bd7b5c51fc489af
AlexandrBlueRose <mr.snow3@yandex.ru>
2018-10-21 01:39:48 +0400

    Added start files and .gitignore

---
+++ Start.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+
+let scrape = async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    //await page.goto('https://habr.com/top')
+    await page.goto('https://vk.com/id271572369');
+    await page.type('#quick_email', '+79859766033');
+    await page.type('#quick_pass', 'fantihon00');
+    await page.click('#quick_login_button');
+    await page.screenshot();
+    await page.waitFor(2000)
+    await browser.close();
+    return 'ok';
+};
+scrape().then((value) => {
+    console.log(value);
+})
