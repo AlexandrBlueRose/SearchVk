c45354b29e37415f8cb53cf4dc1d2648be1f5c58
AlexandrBlueRose <mr.snow3@yandex.ru>
2018-10-26 14:27:39 +0400

    Test and update algoritm

---
+++ engine/scraping.js
@@ -0,0 +1,6 @@
+//Переменная для экспорта функций
+const puppeteer = require("puppeteer");
+var exports = (module.exports = []);
+
+//присоединяем функцию ,чтобы она была видна в других файлах
+//exports.scroll =
